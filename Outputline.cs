ab5a5e8e31ea258ab28eb49ed46f8a8f2468c1eb
CiaranCheng <1095074672@qq.com>
2020-05-20 14:09:15 +0800

    添加项目文件。

---
+++ Outputline.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCsharp
+{
+    class Outputline
+    {
+        public void PrintTime() {
+            Console.WriteLine("It is "+ DateTime.Now.ToString() + " now");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Outputline line1 = new Outputline();
+            line1.PrintTime();
+        }
+    }
+}
